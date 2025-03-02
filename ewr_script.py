d9f7f918f3932c3462edf098ed2f130efdf2f25a
jackcollop <jackcollop@gmail.com>
2025-03-02 17:51:55 -0600

    Update ewr_script.py

--- ewr_script.py
+++ ewr_script.py
@@ -2,6 +2,6 @@ import pandas as pd
 import datetime as dt
 import lxml
 ewr = pd.read_html(r'https://www.ewrinc.com/cotton/contentPublic/reports/stateReceipts.aspx')
-table = ewr[3]
+table = ewr[2]
 date = str(dt.datetime.today())[:10]
 table.to_csv(path_or_buf=f'EWR-{date}.csv')
