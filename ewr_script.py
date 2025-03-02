43c2d0029d46898cdafb2b79426fc50b6897e189
jackcollop <jackcollop@gmail.com>
2025-03-02 17:15:18 -0600

    Update ewr_script.py

--- ewr_script.py
+++ ewr_script.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import datetime as dt
+import lxml
 ewr = pd.read_html(r'https://www.ewrinc.com/cotton/contentPublic/reports/stateReceipts.aspx')
 table = ewr[3]
 date = str(dt.datetime.today())[:10]
