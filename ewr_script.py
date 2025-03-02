f7c62a741614cf8119e9ad110e05d3d033762c9c
jackcollop <jackcollop@gmail.com>
2025-03-02 16:10:04 -0600

    Add files via upload

---
+++ ewr_script.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import datetime as dt
+ewr = pd.read_html(r'https://www.ewrinc.com/cotton/contentPublic/reports/stateReceipts.aspx')
+table = ewr[3]
+date = str(dt.datetime.today())[:10]
+table.to_csv(path_or_buf=f'EWR-{date}.csv')
