ea65434462b2424daac37a479b9e6599a0434277
jackcollop <jackcollop@gmail.com>
2025-05-11 21:13:00 -0500

    Update lit2.py

--- lit2.py
+++ lit2.py
@@ -59,4 +59,4 @@ states.dropna(inplace=True)
 st.subheader("Electronic warehouse receipts")
 st.dataframe(states[['AL & FL', "AR", "AZ & NM",'CA', 'GA',"KS & OK", 'LA', 'MO','MS', "NC & VA", 'SC', 'TN', 'TX']].sort_index())
 
-st.line_chart(states[['AL & FL', "AR", "AZ & NM",'CA', 'GA',"KS & OK", 'LA', 'MO','MS', "NC & VA", 'SC', 'TN', 'TX']].sort_index().rolling(7).sum())
+st.line_chart(states[['Southwest','Far West','Southeast','Mid South']])
