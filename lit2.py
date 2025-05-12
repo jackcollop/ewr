4d390ca9762c2d552d209924cc67c1fada9cd9ae
jackcollop <jackcollop@gmail.com>
2025-05-12 11:28:51 -0500

    Update lit2.py

--- lit2.py
+++ lit2.py
@@ -55,10 +55,10 @@ states.set_index('Date', inplace=True)
 states.dropna(inplace=True)
 
 
-dif = states[['AL & FL', "AR", "AZ & NM",'CA', 'GA',"KS & OK", 'LA', 'MO','MS', "NC & VA", 'SC', 'TN', 'TX']].sort_index().diff()
+dif = states[['Southwest','Far West','Southeast','Mid South']].sort_index().diff()
 
 st.dataframe(dif.groupby(dif.index.isocalendar().week).sum())
-st.line_chart(dif[['Southwest','Far West','Southeast','Mid South]].groupby(dif.index.isocalendar().week).sum().mul(-1))
+st.line_chart(dif[['Southwest','Far West','Southeast','Mid South']].groupby(dif.index.isocalendar().week).sum().mul(-1))
 
 
 st.subheader("Texas")
