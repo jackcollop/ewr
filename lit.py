9f2473546377a4252e86c1146e7ed5cda5d2184e
jackcollop <jackcollop@gmail.com>
2025-08-06 11:14:46 -0500

    Update lit.py

--- lit.py
+++ lit.py
@@ -9,16 +9,18 @@ dates = []
 ccc = []
 so = []
 tot_open = []
+open25 = []
 for file in files:
     f = pd.read_csv(file)
     dates.append(file[-14:-4])
     ccc.append(f.iloc[13,4])
     so.append(f.iloc[13,5])
     tot_open.append(f.iloc[13,10])
+    open25.append(f.iloc[13,5])
 #%%
-ewr = pd.DataFrame([dates,ccc,so, tot_open]).T
+ewr = pd.DataFrame([dates,ccc,so, tot_open, open25]).T
 #%%
-ewr.columns = ['Date', 'CCC', 'SO','Total Open']
+ewr.columns = ['Date', 'CCC', 'SO','Total Open','Open 2025']
 
 
 
@@ -26,3 +28,4 @@ st.subheader("Electronic warehouse receipts")
 st.dataframe(ewr.set_index('Date').sort_index(ascending=False))
 
 st.line_chart(ewr.set_index('Date'))
+
