808e4db0d34ecb1347873c394c56b31e6c58220f
jackcollop <jackcollop@gmail.com>
2026-01-15 12:21:26 -0600

    Update lit.py

--- lit.py
+++ lit.py
@@ -31,7 +31,8 @@ st.dataframe(ewr.set_index('Date').sort_index(ascending=False))
 st.line_chart(ewr.set_index('Date'))
 
 st.subheader("Electronic warehouse receipts delta")
-st.dataframe(ewr.set_index('Date').sort_index(ascending=True).diff().sort_index(ascending=True))
+st.dataframe(ewr.set_index('Date').sort_index(ascending=True).diff().sort_index(ascending=False))
+
 
 
 
