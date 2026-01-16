8950f2975bc45bfd23a18fff942f64145eb3629f
jackcollop <jackcollop@gmail.com>
2026-01-16 12:24:11 -0600

    Update lit.py

--- lit.py
+++ lit.py
@@ -19,7 +19,7 @@ for file in files:
         so.append(f['Under Shipping Order'].iloc[-1])
         tot_open.append(f[f'Total Open'].iloc[-1])
         open25.append(f.iloc[-1,6])
-        new25.append(f.iloc[-1,2])
+        new25.append(f.iloc[-1,3])
 #%%
 ewr = pd.DataFrame([dates,ccc,so, tot_open, open25, new25]).T
 #%%
@@ -34,6 +34,7 @@ st.line_chart(ewr.set_index('Date'))
 
 st.subheader("Electronic warehouse receipts delta")
 st.dataframe(ewr.set_index('Date').sort_index(ascending=True).diff().sort_index(ascending=False))
+
 
 
 
